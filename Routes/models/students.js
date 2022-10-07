ad77985a20ff681cd343f6afea7d806055e6deb6
Libaan-Abdi <libaanzakk@gmail.com>
2022-10-07 15:22:39 +0300

    added student js schema

--- Routes/models/students.js
+++ Routes/models/students.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const studentSchema = new Schema({
-    firstname: {
-        type : String,
-        required:[true,'Firstname is required']
+    Firstname: {
+        type: String,
+        required:[true,'First Name is required']
+    },
+    Lastname: {
+        type: String,
+        required:[true,'Last Name is required']
+    },
+    Gender: {
+        type: String
     }
+
 })
